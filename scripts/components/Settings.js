8afb690eca4a49b45be5032bdb12cffb1298bbf9
Glitch (hello-webpage) <none>
2021-03-18 23:59:07 +0000

    🐡🦄 Checkpoint ./script.js:24872531/4074 ./index.html:24872531/8026 ./scripts/components/Settings.js:24872531/2873 ./scripts/App.js:24872531/4882

---
+++ scripts/components/Settings.js
@@ -0,0 +1,27 @@
+// <div class="SETTINGS" style="width:0%">
+//   <div class="settingsInner">
+//     <div>
+//       <label>pause after line</label>
+//       <input class="pauseLineInput" type="number" step="0.01" value="0.5" />
+//     </div>
+//     <div>
+//       <label>pause after word</label>
+//       <input class="pauseWordInput" type="number" step="0.01" value="0.25" />
+//     </div>
+//     <div>
+//       <label>tempo</label>
+//       <input class="tempoInput" type="number" step="1" value="120" />
+//     </div>
+//     <div>
+//       <label>
+//         loop
+//         <input class="loopButton" type="checkbox" />
+//       </label>
+//     </div>
+//     <div>
+//       <span class="tickIndicator">tick:</span>
+//     </div>
+//   </div>
+//   <div class="credits">dit(.)dah(/) by aceslowman 2021</div>
+//   <div class="toggleSettings"></div>
+// </div>;

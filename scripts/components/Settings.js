d8dfc3254169ab108bdc286db334b4e870e4f319
Glitch (hello-webpage) <none>
2021-03-19 02:28:32 +0000

    🍉📉 Checkpoint ./scripts/components/Settings.js:24872531/2506

--- scripts/components/Settings.js
+++ scripts/components/Settings.js
@@ -59,74 +59,50 @@ const Settings = props => {
     <div className="SETTINGS" style={{ width: expanded ? "300px" : "0%" }}>
       <div className="settingsInner">
         <InputPanel title="basic">
-          <InputRow>
-            <InputGroup>
-              <label>number of bars</label>
-              <input
-                onChange={props.onNumBarsChange}
-                className="numBarsInput"
-                type="number"
-                step="1"
-                value={props.numBars}
-              />
-            </InputGroup>
+          <InputRow>            
             <InputGroup>
               <label>tempo</label>
               <input
                 onChange={props.onBPMChange}
-                className="tempoInput"
                 type="number"
                 step="1"
                 value={props.bpm}
               />
             </InputGroup>
-<!--             pauseLineInput
-pauseWordInput -->
           </InputRow>
           <button onClick={props.onResetMelody}>reset melody</button>
         </InputPanel>
         <InputPanel title="articulation">
+          <InputRow>            
           <InputGroup>
-              <label>tempo</label>
+              <label>pause between lines</label>
               <input
-                onChange={props.onBPMChange}
-                className="tempoInput"
+                onChange={props.onChangePauseAfterLine}
                 type="number"
-                step="1"
-                value={props.bpm}
+                step="0.1"
+                value={props.pauseAfterLine}
               />
             </InputGroup>
+          <InputGroup>
+              <label>pause between words</label>
+              <input
+                onChange={props.onChangePauseAfterWord}
+                type="number"
+                step="0.1"
+                value={props.pauseAfterWord}
+              />
+            </InputGroup>
+          </InputRow>
         </InputPanel>
+        <div className="credits">
+          dit(.)dah(/) by{" "}
+          <a href="https://linktr.ee/aceslowman" target="_blank">
+            aceslowman
+          </a>{" "}
+           &nbsp; 2021
+        </div>
       </div>
       <div className="toggleSettings" onClick={toggleSettings}></div>
     </div>
   );
 };
-
-// <div class="SETTINGS" style="width:0%">
-//   <div class="settingsInner">
-//     <div>
-//       <label>pause after line</label>
-//       <input class="pauseLineInput" type="number" step="0.01" value="0.5" />
-//     </div>
-//     <div>
-//       <label>pause after word</label>
-//       <input class="pauseWordInput" type="number" step="0.01" value="0.25" />
-//     </div>
-//     <div>
-//       <label>tempo</label>
-//       <input class="tempoInput" type="number" step="1" value="120" />
-//     </div>
-//     <div>
-//       <label>
-//         loop
-//         <input class="loopButton" type="checkbox" />
-//       </label>
-//     </div>
-//     <div>
-//       <span class="tickIndicator">tick:</span>
-//     </div>
-//   </div>
-//   <div class="credits">dit(.)dah(/) by aceslowman 2021</div>
-//   <div class="toggleSettings"></div>
-// </div>;

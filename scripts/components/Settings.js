8e6d9dd68593483eb7bc44e73842ac91b3f866b5
Glitch (hello-webpage) <none>
2021-03-19 00:10:08 +0000

    🏟🚉 Checkpoint ./scripts/components/Settings.js:24872531/13196 ./style.css:24872531/4153

--- scripts/components/Settings.js
+++ scripts/components/Settings.js
@@ -1,3 +1,94 @@
+/* global Tone, ReactDOM, React */
+const InputPanel = props => (
+  <div
+    style={{
+      display: "flex",
+      margin: "5px 0px",
+      width: "100%",
+      flexFlow: "column",
+      border: "1px groove #602500",
+      padding: "10px"
+    }}
+  >
+    <h3 style={{ margin: "0px 0px 8px 0px" }}>{props.title}</h3>
+    {props.children}
+  </div>
+);
+
+const InputGroup = props => (
+  <div
+    style={{
+      display: "flex",
+      flexFlow: "column",
+      alignSelf: "flex-end",
+      width: "48%"
+    }}
+  >
+    {props.children}
+  </div>
+);
+
+const InputRow = props => (
+  <div
+    style={{
+      display: "flex",
+      flexFlow: "row",
+      width: "100%",
+      alignItems: "center",
+      justifyContent: "space-between",
+      paddingBottom: "5px"
+    }}
+  >
+    {props.children}
+  </div>
+);
+
+const Settings = props => {
+  let [expanded, setExpanded] = React.useState();
+
+  let style = {
+    width: "0%"
+  };
+
+  // SETTINGS TOGGLE
+  function toggleSettings() {
+    setExpanded(prev => !prev);
+  }
+
+  return (
+    <div className="SETTINGS" style={{ width: expanded ? "300px" : "0%" }}>
+      <div className="settingsInner">
+        <InputPanel title="basic">
+          <InputRow>
+            <InputGroup>
+              <label>number of bars</label>
+              <input
+                onChange={props.onNumBarsChange}
+                className="numBarsInput"
+                type="number"
+                step="1"
+                value={props.numBars}
+              />
+            </InputGroup>
+            <InputGroup>
+              <label>tempo</label>
+              <input
+                onChange={props.onBPMChange}
+                className="tempoInput"
+                type="number"
+                step="1"
+                value={props.bpm}
+              />
+            </InputGroup>
+          </InputRow>
+          <button onClick={props.onResetMelody}>reset melody</button>
+        </InputPanel>
+      </div>
+      <div className="toggleSettings" onClick={toggleSettings}></div>
+    </div>
+  );
+};
+
 // <div class="SETTINGS" style="width:0%">
 //   <div class="settingsInner">
 //     <div>

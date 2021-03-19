bf1a3d2cd56ad191e28c9b05fbe4c6b701b37c78
Glitch (hello-webpage) <none>
2021-03-19 02:05:10 +0000

    ⛲️🐚 Checkpoint ./scripts/App.js:24872531/69850 ./index.html:24872531/152

--- scripts/App.js
+++ scripts/App.js
@@ -3,6 +3,7 @@ const App = () => {
   let [pauseAfterLine, setPauseAfterLine] = React.useState(0.5);
   let [pauseAfterWord, setPauseAfterWord] = React.useState(0.25);
   let [text, setText] = React.useState();
+  let [part, setPart] = React.useState(); // for Tone.js Part
 
   let [ready, setReady] = React.useState();
   let [isPlaying, setIsPlaying] = React.useState();
@@ -27,7 +28,7 @@ const App = () => {
 
     document.addEventListener("keydown", keybindings, false);
     return () => document.removeEventListener("keydown", keybindings, false);
-  }, [handleTogglePlay, /*sequence,*/ setIsPlaying, isPlaying]);
+  }, [handleTogglePlay, /*part,*/ setIsPlaying, isPlaying]);
 
   /*
     startup audio context
@@ -52,9 +53,9 @@ const App = () => {
     };
   }, [ready, synth, setSynth]);
 
-  /*
-    on Mount
-  */
+  // when text changes
+  React.useEffect(() => {}, [text]);
+
   // CENTERS MAININPUT TEXT
   // https://stackoverflow.com/questions/4954252/css-textarea-that-expands-as-you-type-text
   const updateSize = e => {
@@ -93,36 +94,133 @@ const App = () => {
     }
   };
 
+  function restartSynth() {
+    if (part) part.stop();
+
+    let events = getPartFromText();
+
+    // use an array of objects as long as the object has a "time" attribute
+    part = new Tone.Part((time, value) => {
+      // the value is an object which contains both the note and the velocity
+      synth.triggerAttackRelease(
+        value.note,
+        "8n",
+        // value.duration,
+        time,
+        value.velocity
+      );
+    }, events).start(0);
+
+    part.loop = true;
+    // make sure loopEnd is the full length of the parts
+    // console.log('LOOP END', )
+    part.loopEnd =
+      events[events.length - 1].time +
+      Tone.Time(events[events.length - 1].duration).toSeconds();
+
+    Tone.Transport.start();
+
+    console.log("loopStart", part.loopStart);
+    console.log("loopEnd", part.loopEnd);
+  }
+
+  const getPartFromText = () =>{
+    console.group();
+    let isLineEnd,
+      isWordEnd = false;
+
+    let partArray = [];
+    let iter = 0;
+
+    let qTime = Tone.Time("4n").toSeconds();
+
+    // get part from text
+    text.forEach((line, l_i) => {
+      line.forEach((word, w_i) => {
+        word.split("").forEach((unit, u_i) => {
+          let unitTime = qTime * iter;
+          let duration = unit === "." ? "8n" : "4n";
+          let note = unit === "." ? "C3" : "C4";
+          let velocity = unit === "." ? 0.5 : 1.0;
+
+          // scale base time
+          // time 1/= 4;
+
+          // hold for end of line
+          if (isLineEnd && isWordEnd) {
+            console.log("holding at end of line", unit);
+            unitTime += pauseAfterLine * qTime;
+            // duration = "1m";
+          } else if (isWordEnd) {
+            // hold for end of word
+            console.log("holding at end of word...", unit);
+            unitTime += pauseAfterWord * qTime;
+          }
+
+          partArray.push({
+            time: unitTime,
+            duration: "8n",
+            // duration: duration,
+            note: note,
+            velocity: velocity
+          });
+
+          iter++;
+
+          // pause needs to apply to the NEXT note after the line or word
+          isLineEnd = w_i === line.length - 1;
+          isWordEnd = u_i === word.split("").length - 1;
+        });
+      });
+    });
+
+    console.log("partarray", partArray);
+    console.groupEnd();
+
+    return partArray;
+  }
+
   const handleTogglePlay = e => {
     if (isPlaying) {
       Tone.Transport.cancel();
       Tone.Transport.stop();
-      // sequence.cancel();
-      // sequence.stop();
+      // part.cancel();
+      // part.stop();
       setIsPlaying(false);
     } else {
       Tone.Transport.cancel();
-      // sequence.start();
+      // part.start();
       Tone.Transport.start();
       setIsPlaying(true);
     }
-  }
-  
+  };
+
   const handleTextAreaKeyDown = e => {
     filterUserInput(e);
     keyDownUpdateSize(e);
-  }
-  
+  };
+
   const handleTextAreaKeyUp = e => {
     handleMainTextChange(e);
     keyUpUpdateSize(e);
-  }
+  };
+
+  const handleBPMChange = e => setBPM(parseFloat(e.target.value));
+  const handlePauseAfterLine = e => setPauseAfterLine(e.target.value);
+  const handlePauseAfterWord = e => setPauseAfterWord(e.target.value);
 
   return (
     <React.Fragment>
-      <Settings />
+      <Settings
+        onBPMChange={handleBPMChange}
+        onPauseAfterLine={handlePauseAfterLine}
+        onPauseAfterWord={handlePauseAfterWord}
+        bpm={bpm}
+        pauseAfterLine={pauseAfterLine}
+        pauseAfterWord={pauseAfterWord}
+      />
       <textarea
-        class="MAININPUT"
+        className="MAININPUT"
         placeholder="_"
         onKeyUp={handleTextAreaKeyUp}
         onKeyDown={handleTextAreaKeyDown}
@@ -133,110 +231,3 @@ const App = () => {
 
 const domContainer = document.getElementById("APP");
 ReactDOM.render(React.createElement(App), domContainer);
-
-// l/* global Tone */
-// let synth = new Tone.Synth().toDestination();
-// let part;
-
-
-// function restartSynth() {
-//   if(part) part.stop();
-
-//   let events = getPartFromText();
-
-//   // use an array of objects as long as the object has a "time" attribute
-//   part = new Tone.Part((time, value) => {
-//     // the value is an object which contains both the note and the velocity
-//     synth.triggerAttackRelease(
-//       value.note,
-//       "8n",
-//       // value.duration,
-//       time,
-//       value.velocity
-//     );
-//   }, events).start(0);
-
-//   part.loop = true;
-//   // make sure loopEnd is the full length of the parts
-//   // console.log('LOOP END', )
-//   part.loopEnd = events[events.length-1].time + Tone.Time(events[events.length-1].duration).toSeconds()
-
-//   Tone.Transport.start();
-
-//   console.log("loopStart", part.loopStart);
-//   console.log("loopEnd", part.loopEnd)
-// }
-
-// function getPartFromText() {
-//   console.group()
-//   let isLineEnd, isWordEnd = false;
-
-//   let partArray = [];
-//   let iter = 0;
-
-//   let qTime = Tone.Time("4n").toSeconds();
-
-//   // get part from text
-//   state.txtArray.forEach((line, l_i) => {
-//     line.forEach((word, w_i) => {
-//       word.split("").forEach((unit, u_i) => {
-//         let unitTime = qTime * iter;
-//         let duration = unit === "." ? "8n" : "4n";
-//         let note = unit === "." ? "C3" : "C4";
-//         let velocity = unit === "." ? 0.5 : 1.0;
-
-//         // scale base time
-//         // time 1/= 4;
-
-//         // hold for end of line
-//         if (isLineEnd && isWordEnd) {
-//           console.log("holding at end of line", unit);
-//           unitTime += state.pauseAfterLine * qTime;
-//           // duration = "1m";
-//         } else if (isWordEnd) {
-//           // hold for end of word
-//           console.log("holding at end of word...", unit);
-//           unitTime += state.pauseAfterWord * qTime;
-//         }
-
-//         partArray.push({
-//           time: unitTime,
-//           duration: '8n',
-//           // duration: duration,
-//           note: note,
-//           velocity: velocity
-//         });
-
-//         iter++;
-
-//         // pause needs to apply to the NEXT note after the line or word
-//         isLineEnd = w_i === line.length - 1;
-//         isWordEnd = u_i === word.split("").length - 1;
-//       });
-//     });
-//   });
-
-//   console.log("partarray", partArray);
-//   console.groupEnd();
-
-//   return partArray;
-// }
-
-// function onBPMChange(e) {
-//   Tone.Transport.bpm.value = parseFloat(e.target.value);
-// }
-
-
-// function handlePauseAfterLine(e) {
-//   state.pauseAfterLine = e.target.value;
-//   restartSynth();
-// }
-
-// function handlePauseAfterWord(e) {
-//   state.pauseAfterWord = e.target.value;
-//   restartSynth();
-// }
-
-// function handleLoopToggle(e) {
-
-// }
